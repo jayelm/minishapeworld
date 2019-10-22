c87baf0722909f59f705cc444ded2f972c8e3139
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2019-10-22 14:20:02 -0700

    Create language file

--- minishapeworld.py
+++ minishapeworld.py
@@ -14,6 +14,7 @@ import shape
 import config
 import constants as c
 import vis
+import lang
 
 
 class MiniShapeWorld:
@@ -110,7 +111,7 @@ class MiniShapeWorld:
         if float_type:
             all_imgs = np.divide(all_imgs, 255.0)
             all_labels = all_labels.astype(np.float32)
-        langs = np.array([fmt_config(c) for c in configs], dtype=np.unicode)
+        langs = np.array([lang.fmt_config(c) for c in configs], dtype=np.unicode)
 
         if self.data_type == 'caption':
             # Squeeze out the images per example dim
@@ -433,58 +434,6 @@ class MiniShapeWorld:
         while new_s == existing_shape:
             new_s = random.choice(self.shapes)
         return new_s
-
-
-def fmt_config(cfg):
-    if isinstance(cfg, config.SingleConfig):
-        return _fmt_config_single(cfg)
-    elif isinstance(cfg, config.SpatialConfig):
-        return _fmt_config_spatial(cfg)
-    else:
-        raise NotImplementedError(type(cfg))
-
-
-def _fmt_config_single(cfg):
-    color_, shape_ = cfg
-    shape_txt = 'shape'
-    color_txt = ''
-    if shape_ is not None:
-        shape_txt = shape_
-    if color_ is not None:
-        color_txt = color_ + ' '
-    return '{}{}'.format(color_txt, shape_txt)
-
-
-def _fmt_config_spatial(cfg):
-    (s1, s2), relation, relation_dir = cfg
-    if relation == 0:
-        if relation_dir == 0:
-            rel_txt = 'left'
-        else:
-            rel_txt = 'right'
-    else:
-        if relation_dir == 0:
-            rel_txt = 'below'
-        else:
-            rel_txt = 'above'
-    if s1[0] is None:
-        s1_0_txt = ''
-    else:
-        s1_0_txt = s1[0]
-    if s1[1] is None:
-        s1_1_txt = 'shape'
-    else:
-        s1_1_txt = s1[1]
-    if s2[0] is None:
-        s2_0_txt = ''
-    else:
-        s2_0_txt = s2[0]
-    if s2[1] is None:
-        s2_1_txt = 'shape'
-    else:
-        s2_1_txt = s2[1]
-    return '{} {} {} {} {}'.format(s1_0_txt, s1_1_txt, rel_txt, s2_0_txt,
-                                   s2_1_txt)
 
 
 if __name__ == '__main__':
