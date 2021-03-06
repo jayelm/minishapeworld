0ca41e03a93dcc686d801ddd2cd8d2b9e44d36c3
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2021-03-06 13:54:42 -0800

    fix logical all dset

--- logical_all.sh
+++ logical_all.sh
@@ -2,9 +2,6 @@
 
 python -m msw.generate --n_images 40 --n_correct 20 \
     --config_type logical \
-    --logical_ops and \
-    --min_logical_len 2 \
-    --max_logical_len 2 \
     --enumerate_configs \
     --n_train 10000 --n_val 1000 --n_val_same 1000 --n_test 1000 --n_test_same 1000 \
     --save_dir logical_all --vis
