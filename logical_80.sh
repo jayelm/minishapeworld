15d0037b7b7019c6d54588faad0d7cb88ca3e1ce
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2021-04-08 02:23:40 -0700

    80ex

---
+++ logical_80.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python -m msw.generate --n_images 80 --n_correct 40 \
+    --config_type logical \
+    --config_split --gen_same \
+    --enumerate_configs \
+    --n_train 10000 --n_val 1000 --n_val_same 1000 --n_test 1000 --n_test_same 1000 \
+    --save_dir logical_80 --vis
