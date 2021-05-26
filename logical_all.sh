f55a62d86cfbf6bf50b1d69de91babd6acfad70f
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2021-05-25 20:58:46 -0700

    rm logical

--- logical_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python -m msw.generate --n_images 40 --n_correct 20 \
-    --config_type logical \
-    --enumerate_configs \
-    --n_train 10000 --n_val 1000 --n_val_same 1000 --n_test 1000 --n_test_same 1000 \
-    --save_dir logical_all --vis --hdf5
--- logical_hardneg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python -m msw.generate --n_images 40 --n_correct 20 \
-    --config_type logical \
-    --config_split --gen_same \
-    --enumerate_configs \
-    --n_train 20000 --n_val 1000 --n_val_same 1000 --n_test 1000 --n_test_same 1000 \
-    --color_variance 0.02 \
-    --save_dir logical_hardneg --hdf5 --vis
