9ad43d301d7b05c9afaeacf31bbd081b6d071ab6
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2021-05-25 20:58:19 -0700

    emergent generalization dataset scripts

---
+++ emergent_generalization_shapeworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python -m msw.generate --n_images 40 --n_correct 20 \
+    --config_type logical \
+    --config_split --gen_same \
+    --enumerate_configs \
+    --n_train 20000 --n_val 1000 --n_val_same 1000 --n_test 1000 --n_test_same 1000 \
+    --color_variance 0.02 \
+    --oversample_shape \
+    --oversample_shape_strategy even \
+    --save_dir shapeworld --hdf5 --vis
